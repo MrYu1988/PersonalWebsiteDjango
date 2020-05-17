4e94c82aa298c6c4ab428253bdb8b629fad7aba1
yu <taideng_1314@126.com>
2020-05-17 23:19:40 +0800

    主页不现实照片墙，只显示卡片，life页面也只显示照片

--- PersonalWebsiteDjango/settings.py
+++ PersonalWebsiteDjango/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'Ability',
     'Article',
     'Program',
+    'Life',
 ]
 
 MIDDLEWARE = [
--- app/Life/urls.py
+++ app/Life/urls.py
@@ -13,17 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from Life.views import LifeDisplay
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', include('index.urls')),
-    path('', include('index.urls')),
-    path('program/', include('Program.urls')),
-    path('article/', include('Article.urls')),
-    path('ability/', include('Ability.urls')),
-    path('life/', include('Life.urls')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('', LifeDisplay.as_view(), name='life'),
+]
